cb7bbe3961d328a385b419cacfc8254854dbcf73
Monwar <monwar259@gmail.com>
2021-12-17 13:48:01 +0000

    changed next auth file

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,24 +7,24 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-
     }),
     // ...add more providers here
   ],
-  theme: {
-    logo: "https://links.papareact.com/sq8",
-    brandColor: "#F13287",
-    colorScheme: "auto"
-  },
+  // theme: {
+  //   logo: "https://links.papareact.com/sq8",
+  //   brandColor: "#F13287",
+  //   colorScheme: "auto"
+  // },
   pages: {
     signIn: "/auth/signin"
   },
   callbacks: {
     async session({ session, token, user }) {
-      session.user.username = session.user.name.split(" ").join("_").toLowerCase();
-
+      session.user.username = session.user.name
+        .split(" ")
+        .join("")
+        .toLowerCase();
       session.user.uid = token.sub;
-
       return session;
     }
   }
